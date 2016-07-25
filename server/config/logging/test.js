ce1ebf5dc877f331c0b93d22f245f5a703f97265
tanerochris <tanejuth@gmail.com>
2016-07-25 05:41:02 +0100

    updated  winston

--- server/config/logging/test.js
+++ server/config/logging/test.js
@@ -5,5 +5,5 @@ var log = require("./winston");
 
 log.info("hello info");
 log.warning("hello warning");
-log.debug("hello debug");
+//log.debug("hello debug");
 log.error("An error");
--- server/config/logging/winston.js
+++ server/config/logging/winston.js
@@ -1,41 +1,99 @@
 /*Winston setup file*/
 /*set up log path*/
-/*create log api for logging*/
+/*work to complete */
+/*Work on formatter function to produce msg pattern: %d{dd/MM/yyyy HH:mm:ss} %p - %m%n*/
+/*Log rotate files */
 
 "use strict"
 const winston = require("winston");
 const fs = require('fs');
 const path = require("path");
 const dirPath = path.dirname(path.dirname(__dirname));
-const fullPath = dirPath + "/logs";
-//define paths of log files
-const debug = fullPath + "/debug.log";
-const warning = fullPath + "/warning.log";
-const error = fullPath + "/error.log";
-const info = fullPath +"/info.log";
+const fullPath = path.join(dirPath,"/logs");
+const debug = false;
 var logDirExist = false;
 
-exports.debug = function(){
-	
-	return log(debug ,"debug");
-};
-exports.warning = function(){
-	
-	return log(warning ,"warn");
-};
-exports.info = function(){
-	
-	return log(info ,"info");
-};
-exports.error = function(){
-	
-	return log(error ,"error");
-};
+//setting up logger
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+        	name :'file-info',
+            level: 'info',
+            filename: '../../logs/info.log',
+            handleExceptions: true,
+            json: false,
+            maxSize: 10485760, 
+            maxFiles: 10,
+            prettyPrint :true,
+            formatter : formatter
+            
+        }),
+        new winston.transports.File({
+        	name :'file-debug',
+            level: 'debug',
+            filename: '../../logs/debug.log',
+            handleExceptions: true,
+            json: false,
+            maxSize: 10485760, 
+            maxFiles: 10,
+            prettyPrint :true,
+            formatter : formatter
+            
+        }),
+         new winston.transports.File({
+         	name :'file-error',
+            level: 'error',
+            filename: '../../logs/error.log',
+            handleExceptions: true,
+            json: false,
+            maxSize: 10485760, 
+            maxFiles: 10,
+            prettyPrint :true,
+            formatter : formatter
+            
+        }),
+          new winston.transports.File({
+            name :'file-warn',
+            level: 'warn',
+            filename: '../../logs/warnings.log',
+            handleExceptions: true,
+            json: false,
+            maxSize: 10485760, 
+            maxFiles: 10,
+            prettyPrint :true,
+            formatter : formatter
+           
+        })
+    ],
+    exitOnError: false
+});
+
+
+var log = {
+	debug : function(msg){
+		logFxn(logger,msg,"debug");
+	},
+	info : function(msg){
+		logFxn(logger,msg,"info");
+	},
+	warning : function(msg){
+		logFxn(logger,msg,"warn");
+	},
+	error : function(msg){
+		logFxn(logger,msg,"error");
+	}
+}
+
+var formatter = function(options){
+	return options.timestamp() +' ['+ options.level.toUpperCase() +'] '+ (undefined !== options.message ? options.message : '') +
+     (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+}
 
 var setup = function(){
 	
 	if (!fs.existsSync(fullPath)) {
       fs.mkdirSync(fullPath);
+      logDirExist = true;
 }else{
 	logDirExist = true;
 }
@@ -47,33 +105,32 @@ var setup = function(){
   }
   
 };
- var log = function(path ,level){
- 	if(setup()){
-    fs.openSync(path , "a+");
-    console.log(path);
-     var logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.File)({ filename: path ,level: level })
-    ]
-  });
-     switch(level){
+ var logFxn = function(logger,msg,level){
+      if(setup()){
+      	switch(level){
      	case "info":
-     		return logger.info;
+            
+     		logger.info(msg);
      		break;
      	case "debug":
-     		return logger.debug;
-     		break;
+     	   if(debug){
+     		 logger.debug(msg);
+     		}else{//exception will be added subsequently
+     			
+     			logger.log(msg);
+     		}
+     	    break
      	case "error":
-     		return logger.error;
+     		logger.error(msg);
      		break;
      	case "warning":
-     	     return logger.warn;
+     	    logger.warn(msg);
      	     break;
      	default:
-     	  return logger.log;
+     	    logger.log(msg);
      }
-	}
-    else{
-    	return winston.log;
-    }
+
+      }
+     
 }
+module.exports = log;
